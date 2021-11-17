3d34ee1738483f156c646323bebd1d9c4cdf4be5
heavensbee001 <heavensbee001@gmail.com>
2021-11-17 19:23:00 +0100

    Fix setup

--- packages/hardhat/contracts/YourToken.sol
+++ packages/hardhat/contracts/YourToken.sol
@@ -1,6 +1,7 @@
-pragma solidity >=0.6.0 <0.7.0;
+// SPDX-License-Identifier: MIT
+pragma solidity <0.9.0;
 
-//import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 // learn more: https://docs.openzeppelin.com/contracts/3.x/erc20
 
 contract YourToken {
