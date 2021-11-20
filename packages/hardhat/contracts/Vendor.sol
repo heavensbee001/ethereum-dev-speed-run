c1f0c4297364f49fe26b84f4bbd08dc2632e3318
heavensbee001 <heavensbee001@gmail.com>
2021-11-20 11:37:06 +0100

    Challenge 4:
    sell tokens

--- packages/hardhat/contracts/Vendor.sol
+++ packages/hardhat/contracts/Vendor.sol
@@ -28,6 +28,16 @@ contract Vendor is Ownable{
   }
 
   //ToDo: create a sellTokens() function:
+  function sellTokens(uint tokenAmount) payable external {
+    require(tokenAmount > 0, "Specify an amount of token greater than zero");
+    require(yourToken.balanceOf(msg.sender) >= tokenAmount, "You don't have enough tokens");
+    require(address(this).balance >= tokenAmount/tokensPerEth, "Vendor has not enough ETH");
+    (bool sent) = yourToken.transferFrom(msg.sender, address(this), tokenAmount);
+    require(sent, "Failed to transfer tokens to vendor");
+    (sent,) = msg.sender.call{value: tokenAmount/tokensPerEth}("");
+    require(sent, "Failed to transfer eth from owner to user");
+
+  }
 
   //ToDo: create a withdraw() function that lets the owner, you can 
   //use the Ownable.sol import above:
